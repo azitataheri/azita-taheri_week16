0f61448178b046026cde752b13baf76654039ace
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-02-17 17:38:56 +0330

    change some style sof input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,28 @@
 import { useState } from "react";
-import styles from '../components/Input.module.css'
-import cities from '../cities.json'
+import cities from "../cities.json";
 
-
+import styles from "../components/Input.module.css";
 
 function Input() {
-  const [userInput, setUserInput] = useState("")
-  const [suggestion, setSuggestion] = useState("")
-
+  const [userInput, setUserInput] = useState("");
+  const [suggestion, setSuggestion] = useState("");
 
   const inputHandler = (e) => {
-    const value = e.target.value
-    setUserInput(value)
-
-
-    const match = cities.find((city) => city.toLowerCase().startsWith(value.toLowerCase()) )
-
-    setSuggestion(match && match !== value ? match : '')
-  }
-
+    const value = e.target.value;
+    setUserInput(value);
+    const match = cities.find((city) =>
+      city.toLowerCase().startsWith(value.toLowerCase()),
+    );
+    setSuggestion(match && match !== value ? match : "");
+  };
 
   const handleKeyDown = (e) => {
-    if( e.key === 'Tab' && suggestion){
-      e.preventDefault()
-      setUserInput(suggestion)
-      setSuggestion('')
+    if (e.key === "Tab" && suggestion) {
+      e.preventDefault();
+      setUserInput(suggestion);
+      setSuggestion("");
     }
-  }
+  };
 
   return (
     <div className={styles.autocompletewrapper}>
@@ -42,11 +38,8 @@ function Input() {
           className={styles.input}
         />
         <span className={styles.inputborder}></span>
-      {
-        suggestion && <div className={styles.suggest}>{suggestion}</div>
-      }
+        {suggestion && <div className={styles.suggest}>{suggestion}</div>}
       </div>
-      
     </div>
   );
 }
